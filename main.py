ae5dce0c9f6c22afc71c889e7b9b7e8ed5202237
TaeUpNext <Armontaeh17@gmail.com>
2023-07-24 22:21:06 -0400

    finished READFILE working on EXTRACTZIP

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
+#Imports requtired librareis 
 import tkinter as tk
 from tkinter import filedialog as fd
 from tkinter import *
+from zipfile import ZipFile as zf
+
 print(f'\n This is Taés File Management Automaiton\n ')
 
+#reads and prints the content of the file 
+def READFILE(): 
+  starterFile = fd.askopenfilename()#file/folder you want to change  
+  fileRead = open(starterFile, 'r')
+  print(f'\n {fileRead.read()} \n')
 
-starterFile = fd.askopenfilename()#file/folder you want to change
+#unzips file
+def EXTRACTZIP():
+  fileOrDirectory = int(input("Do you want to extract a zip file or folder? \n1. File: \n2. Folder: \n\nYour Choice: "))
+  if fileOrDirectory == 1:
+    starterFile = fd.askopenfilename() #file/folder you want to extract
+    zip.extract(starterFile)
+  
+  elif fileOrDirectory ==2:
+    starterFolder = fd.askdirectory()
 
-#print/read the contents of a file
-fileRead = open(starterFile, 'r')
 
-print(f'\n {fileRead.read()} \n')
+managerChoice = int(input('What would you like to do today? \n\n1. Read Contents of File: \n2. Extract Zi File \n\nYour Choice:  '))
+if managerChoice == 1:
+  READFILE()
+
+elif managerChoice ==2:
+  EXTRACTZIP()
